ec2e002f620cf567542f508be6bcea1f02f55a13
Like_Abbos <atammura.shin@gmail.com>
2022-12-07 17:11:44 +0300

    HW5 ready

--- Seminars/S5/HW.java
+++ Seminars/S5/HW.java
@@ -17,14 +17,23 @@ public class HW {
         keys.forEach(i -> colors.compute(i, (k,v) -> add_excl(v)));
         colors.forEach((k,v)-> System.out.print(k+" - "+v+"\n"));
         System.out.println("Создаём дерево...");
-        TreeMap<Integer, String> t_colors = (TreeMap<Integer, String>) colors.clone();
-        /*-----------ex4----------------*/
-        System.out.println("Увеличиваем словари до 1000 элементов...");
+        TreeMap<Integer, String> t_colors = new TreeMap<>();
+        colors.forEach((k,v)->t_colors.put(k,v));
+        /*-----------ex4-&-5----------------*/
+        System.out.println("Увеличиваем таблицы до 1000 элементов...");
+        long start = System.currentTimeMillis();
         while (colors.size() < 1000)
             colors.putIfAbsent(new Random().nextInt(10_000),"new_element");
+        long end = System.currentTimeMillis();
+        long t1 = end-start;
+        start = System.currentTimeMillis();
         while (t_colors.size() < 1000)
             t_colors.putIfAbsent(new Random().nextInt(10_000),"new_element");
+        end = System.currentTimeMillis();
+        long t2 = end-start;
 
+        System.out.printf("Время для Словаря: %dмс\n", t1);
+        System.out.printf("Время для Дерева: %dмс\n", t2);
     }
     private static String add_excl (String s) {return s+"!";}
 }
